4245de71caf6374fae7ca93db140e610aa2e7c00
Johan Ostrand <muztanger@gmail.com>
2020-12-01 06:18:15 +0100

    Day1 cleanup

--- AdventOfCode2020/Day01.cs
+++ AdventOfCode2020/Day01.cs
@@ -8,62 +8,13 @@ namespace AdventOfCode2020
 {
     public class Day01
     {
-        /* After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
-
-The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
-
-To save your vacation, you need to get all fifty stars by December 25th.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
-
-Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-
-For example, suppose your expense report contained the following:
-
-1721
-979
-366
-299
-675
-1456
-
-In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-
-Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
-
-To begin, get your puzzle input.
-
-Answer:
-You can also [Shareon Twitter Mastodon] this puzzle. */
-        [Test]
-        public void Part1_Example1()
-        {
-        }
-
-        [Test]
-        public void Part1_Example2()
-        {
-        }
-
         [Test]
         public void Part1()
         {
             var numbers = Common.DayInput(nameof(Day01)).Select(x => int.Parse(x)).ToList();
             for (int i = 0; i < numbers.Count - 1; i++)
-                for (int j = i + 1; j < numbers.Count; j++)
-                    if (numbers[i] + numbers[j] == 2020) Console.WriteLine(numbers[i] * numbers[j]);
-        }
-
-        [Test]
-        public void Part2_Example1()
-        {
-        }
-
-        [Test]
-        public void Part2_Example2()
-        {
+            for (int j = i + 1; j < numbers.Count; j++)
+                if (numbers[i] + numbers[j] == 2020) Console.WriteLine(numbers[i] * numbers[j]);
         }
 
         [Test]
@@ -71,10 +22,9 @@ You can also [Shareon Twitter Mastodon] this puzzle. */
         {
             var numbers = Common.DayInput(nameof(Day01)).Select(x => int.Parse(x)).ToList();
             for (int i = 0; i < numbers.Count - 2; i++)
-                for (int j = i + 1; j < numbers.Count - 1; j++)
-                for (int k = j + 1; k < numbers.Count; k++)
-                        if (numbers[i] + numbers[j] + numbers[k] == 2020) Console.WriteLine(numbers[i] * numbers[j] * numbers[k]);
+            for (int j = i + 1; j < numbers.Count - 1; j++)
+            for (int k = j + 1; k < numbers.Count; k++)
+                if (numbers[i] + numbers[j] + numbers[k] == 2020) Console.WriteLine(numbers[i] * numbers[j] * numbers[k]);
         }
-
     }
 }
