464e2b46821bc6e3065d07e619a5588abc164a3d
Johan Ostrand <muztanger@gmail.com>
2020-12-02 00:09:04 +0100

    Day01 part1 with linq

--- AdventOfCode2020/Day01.cs
+++ AdventOfCode2020/Day01.cs
@@ -11,11 +11,28 @@ namespace AdventOfCode2020
         [Test]
         public void Part1()
         {
+            Assert.AreEqual(877971, Part1Sol1());
+            Assert.AreEqual(877971, Part1Sol2());
+        }
+
+        private static int Part1Sol1()
+        {
             var numbers = Common.DayInput(nameof(Day01)).Select(x => int.Parse(x)).ToList();
             for (int i = 0; i < numbers.Count - 1; i++)
-            for (int j = i + 1; j < numbers.Count; j++)
-                if (numbers[i] + numbers[j] == 2020) Console.WriteLine(numbers[i] * numbers[j]);
+                for (int j = i + 1; j < numbers.Count; j++)
+                    if (numbers[i] + numbers[j] == 2020) return numbers[i] * numbers[j];
+            return -1;
         }
+
+        private static int Part1Sol2()
+        {
+            var numbers = Common.DayInput(nameof(Day01)).Select(x => int.Parse(x)).ToList();
+            return (from l1 in numbers
+                    from l2 in numbers
+                    where l1 < l2 && l1 + l2 == 2020
+                    select l1 * l2).First();
+        }
+
 
         [Test]
         public void Part2()
