2bce21b6131925aa4f766e0600311def18e61a5f
Johan Ostrand <muztanger@gmail.com>
2020-12-16 05:54:47 +0100

    Day15 Part2

--- AdventOfCode2020/Day15.cs
+++ AdventOfCode2020/Day15.cs
@@ -8,17 +8,34 @@ namespace AdventOfCode2020
 {
     public class Day15
     {
+        public class LastTwo
+        {
+            bool flip = true;
+            long count = 0;
+            long[] values = new long[2];
+            
+            public void Add(long x)
+            {
+                values[flip ? 0 : 1] = x;
+                flip = !flip;
+                count++;
+            }
+
+            public long Dist() => Math.Abs(values[0] - values[1]);
+
+            public bool IsTwo() => count >= 2;
+        }
         public class MemoryGame
         {
-            private int[] init;
-            private int spoken;
-            public int Spoken { get => spoken; }
-            int turn = 0;
-            DefaultValueDictionary<int, List<int>> mem;
+            private long[] init;
+            private long spoken;
+            public long Spoken { get => spoken; }
+            long turn = 0;
+            DefaultValueDictionary<long, LastTwo> mem;
             public MemoryGame(string[] init)
             {
-                this.init = init.Select(x => int.Parse(x)).ToArray();
-                this.mem = new DefaultValueDictionary<int, List<int>>(() => new List<int>());
+                this.init = init.Select(x => long.Parse(x)).ToArray();
+                this.mem = new DefaultValueDictionary<long, LastTwo>(() => new LastTwo());
             }
 
             public void Turn()
@@ -31,20 +48,17 @@ namespace AdventOfCode2020
                 }
                 else
                 {
-                    if (mem[spoken].Count < 2)
+                    if (!mem[spoken].IsTwo())
                     {
                         spoken = 0;
                         mem[spoken].Add(turn);
                     }
                     else
                     {
-                        spoken = Math.Abs(mem[spoken].Last() - mem[spoken][mem[spoken].Count - 2]);
+                        spoken = mem[spoken].Dist();
                         mem[spoken].Add(turn);
                     }
                 }
-
-                Console.WriteLine($"{turn}: {spoken}");
-
             }
 
             public static MemoryGame Parse(IEnumerable<string> input)
@@ -71,7 +85,7 @@ namespace AdventOfCode2020
         [TestCase("2,3,1", 78)]
         [TestCase("3,2,1", 438)]
         [TestCase("3,1,2", 1836)]
-        public void Part1_Example2(string input, int expected)
+        public void Part1_Example2(string input, long expected)
         {
             var game = MemoryGame.Parse(Common.GetLines(input));
             for (int i = 0; i < 2020; i++)
@@ -89,30 +103,35 @@ namespace AdventOfCode2020
             {
                 game.Turn();
             }
-            Assert.AreEqual(0, game.Spoken);
+            Assert.AreEqual(447L, game.Spoken);
         }
 
-        [Test]
-        public void Part2_Example1()
+        [TestCase("0,3,6", 175594)]
+        [TestCase("1,3,2", 2578)]
+        [TestCase("2,1,3", 3544142)]
+        [TestCase("1,2,3", 261214)]
+        [TestCase("2,3,1", 6895259)]
+        [TestCase("3,2,1", 18)]
+        [TestCase("3,1,2", 362)]
+        public void Part2_Example1(string input, long expected)
         {
-            string input = @"";
-            //var parsed = Parse(Common.GetLines(input));
-            Assert.AreEqual(0, 1);
-        }
-
-        [Test]
-        public void Part2_Example2()
-        {
-            string input = @"";
-            //var parsed = Parse(Common.GetLines(input));
-            Assert.AreEqual(0, 1);
+            var game = MemoryGame.Parse(Common.GetLines(input));
+            for (int i = 0; i < 30000000; i++)
+            {
+                game.Turn();
+            }
+            Assert.AreEqual(expected, game.Spoken);
         }
 
         [Test]
         public void Part2()
         {
-            //var parsed = Parse(Common.DayInput(nameof(Day15)));
-            Assert.AreEqual(0, 1);
+            var game = MemoryGame.Parse(Common.DayInput(nameof(Day15)));
+            for (int i = 0; i < 30000000; i++)
+            {
+                game.Turn();
+            }
+            Assert.AreEqual(11721679L, game.Spoken);
         }
 
     }
