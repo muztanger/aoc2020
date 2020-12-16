37fd63283882db0a9ecb8a998651ba11ea2d662d
Johan Ostrand <muztanger@gmail.com>
2020-12-16 20:50:48 +0100

    Day16 Part2 Oh boy...

--- AdventOfCode2020/Day16.cs
+++ AdventOfCode2020/Day16.cs
@@ -33,7 +33,7 @@ namespace AdventOfCode2020
 
             public bool IsValid(int x) => r1.InRange(x) || r2.InRange(x);
         }
-        enum State { Init, YourTicket, Nearby };
+        enum State { Init, Your, Nearby };
         private static int Parse(IEnumerable<string> input)
         {
             var rules = new Dictionary<string, FieldRule>();
@@ -43,7 +43,7 @@ namespace AdventOfCode2020
             {
                 if (line.StartsWith("your ticket:"))
                 {
-                    state = State.YourTicket;
+                    state = State.Your;
                 }
                 else if (line.StartsWith("nearby tickets:"))
                 {
@@ -93,6 +93,144 @@ namespace AdventOfCode2020
             return invalidFields.Sum();
         }
 
+        private static long Parse2(IEnumerable<string> input)
+        {
+            var rules = new Dictionary<string, FieldRule>();
+            var state = State.Init;
+            var invalidFields = new List<int>();
+            var myTicket = new List<int>();
+            var validTickets = new List<List<int>>();
+            foreach (var line in input)
+            {
+                if (line.StartsWith("your ticket:"))
+                {
+                    state = State.Your;
+                }
+                else if (line.StartsWith("nearby tickets:"))
+                {
+                    state = State.Nearby;
+                }
+                else if (line.Length == 0)
+                {
+                    // do nothing
+                }
+                else
+                {
+                    if (state == State.Init)
+                    {
+                        // departure location: 45-609 or 616-954
+                        var ruleLine = line.Split(":");
+                        var name = ruleLine[0];
+                        ruleLine = ruleLine[1].Trim().Split();
+                        var r1 = ruleLine[0].Split("-");
+                        var range1 = new RuleRange(int.Parse(r1[0]), int.Parse(r1[1]));
+                        var r2 = ruleLine[2].Split("-");
+                        var range2 = new RuleRange(int.Parse(r2[0]), int.Parse(r2[1]));
+                        var fieldRule = new FieldRule(range1, range2);
+                        rules[name] = fieldRule;
+                    }
+                    else if (state == State.Your)
+                    {
+                        myTicket = line.Split(",").Select(x => int.Parse(x)).ToList();
+                    }
+                    else if (state == State.Nearby)
+                    {
+                        var values = line.Split(",").Select(x => int.Parse(x)).ToList();
+                        int validCount = 0;
+                        foreach (var value in values)
+                        {
+                            var isFoundValid = false;
+                            foreach (var rule in rules.Values)
+                            {
+                                if (rule.IsValid(value))
+                                {
+                                    isFoundValid = true;
+                                    break;
+                                }
+                            }
+                            if (!isFoundValid)
+                            {
+                                invalidFields.Add(value);
+                            }
+                            else
+                            {
+                                validCount++;
+                            }
+                        }
+                        if (validCount == values.Count)
+                        {
+                            validTickets.Add(values);
+                        }
+                    }
+                }
+            }
+            var validRules = new List<List<string>>();
+            for (int i = 0; i < myTicket.Count; i++)
+            {
+                validRules.Add(rules.Keys.ToList());
+            }
+
+            // Remove all invalid
+            foreach (var ticket in validTickets)
+            {
+                for (int i = 0; i < validRules.Count; i++)
+                {
+                    var remove = new HashSet<string>();
+                    foreach (var ruleKV in rules)
+                    {
+                        if (!ruleKV.Value.IsValid(ticket[i]))
+                        {
+                            remove.Add(ruleKV.Key);
+                        }
+                    }
+                    foreach (var r in remove)
+                    {
+                        if (validRules[i].Contains(r))
+                        {
+                            validRules[i].Remove(r);
+                        }
+                    }
+                }
+            }
+
+            var finalRules = new string[validRules.Count];
+
+            // Exclude
+            bool loop = true;
+            while (loop)
+            {
+                loop = false;
+                for (int i = 0; i < validRules.Count; i++)
+                {
+                    if (finalRules[i] == null)
+                    {
+                        if (validRules[i].Count == 1)
+                        {
+                            for (int j = 0; j < validRules.Count; j++)
+                            {
+                                if (i != j)
+                                {
+                                    validRules[j].Remove(validRules[i].First());
+                                    loop = true;
+                                }
+                            }
+                            finalRules[i] = validRules[i].First();
+                        }
+                    }
+                }
+            }
+            var prod = 1L;
+            for (int i = 0; i < finalRules.Length; i++)
+            {
+                if (finalRules[i].StartsWith("departure"))
+                {
+                    prod *= myTicket[i];
+                }
+            }
+
+            return prod;
+        }
+
         [Test]
         public void Part1_Example1()
         {
@@ -130,24 +268,27 @@ nearby tickets:
         [Test]
         public void Part2_Example1()
         {
-            string input = @"";
-            var parsed = Parse(Common.GetLines(input));
-            Assert.AreEqual(0, 1);
-        }
+            string input = @"class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
 
-        [Test]
-        public void Part2_Example2()
-        {
-            string input = @"";
-            var parsed = Parse(Common.GetLines(input));
-            Assert.AreEqual(0, 1);
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9";
+            var parsed = Parse2(Common.GetLines(input));
+            //Assert.AreEqual(12*11*13, parsed);
+            Assert.AreEqual(1, parsed);
         }
 
         [Test]
         public void Part2()
         {
-            var parsed = Parse(Common.DayInput(nameof(Day16)));
-            Assert.AreEqual(0, 1);
+            var parsed = Parse2(Common.DayInput(nameof(Day16)));
+            Assert.AreEqual(0, parsed);
         }
 
     }
