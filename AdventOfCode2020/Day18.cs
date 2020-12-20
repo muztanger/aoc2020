12a154e44bbb5b5d88f287c970143eb2cddeafcf
Johan Ostrand <muztanger@gmail.com>
2020-12-20 23:55:21 +0100

    Day18 part1

--- AdventOfCode2020/Day18.cs
+++ AdventOfCode2020/Day18.cs
@@ -78,6 +78,11 @@ namespace AdventOfCode2020
         {
             public Expression group;
 
+            public Group(Expression broup)
+            {
+                this.group = broup;
+            }
+
             public override long Eval()
             {
                 return group.Eval();
@@ -86,13 +91,13 @@ namespace AdventOfCode2020
 
         private static IEnumerable<MatchCollection> Tokenize(IEnumerable<string> input)
         {
-            return input.Select(line => Regex.Matches(line, @"[\d()+*]+"));
+            return input.Select(line => Regex.Matches(line, @"[\d]+|[()+*]"));
         }
 
         [TestCase("1 + 2 * 3 + 4 * 5 + 6", 71)]
         [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 51)]
         [TestCase("2 * 3 + (4 * 5)", 26)]
-        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3", 437)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
         [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
         [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
         public void Part1_Example1(string input, long expected)
@@ -143,8 +148,27 @@ namespace AdventOfCode2020
                 else if (m.Value.Equals(")"))
                 {
                     var last = stack.Pop();
-                    var group = groups.Pop();
-
+                    stack = groups.Pop();
+                    if (stack.Count > 0)
+                    {
+                        var peek = stack.Peek();
+                        switch (peek)
+                        {
+                            case Addition add:
+                                add.e2 = new Group(last);
+                                break;
+                            case Multiplication mult:
+                                mult.e2 = new Group(last);
+                                break;
+                            default:
+                                stack.Push(new Group(last));
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(new Group(last));
+                    }
                 }
                 else
                 {
@@ -155,19 +179,104 @@ namespace AdventOfCode2020
             Assert.AreEqual(expected, result.Eval());
         }
 
-        [Test]
-        public void Part1_Example2()
-        {
-            string input = @"";
-            var parsed = Tokenize(Common.GetLines(input));
-            Assert.AreEqual(0, 1);
-        }
+        //[TestCase("1 + 2 * 3 + 4 * 5 + 6", 71)]
+        //[TestCase("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        //[TestCase("2 * 3 + (4 * 5)", 26)]
+        //[TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3", 437)]
+        //[TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+        //[TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        //public void Part1_Example1_Simplified(string input, long expected)
+        //{
+        //    var parsed = Tokenize(Common.GetLines(input));
+        //    MatchCollection tokens = parsed.First();
+        //    var stack = new Stack<int>();
+        //    foreach (Match m in tokens)
+        //    {
+        //        Assert.AreEqual(0, 1);
+        //}
 
         [Test]
         public void Part1()
         {
             var parsed = Tokenize(Common.DayInput(nameof(Day18)));
-            Assert.AreEqual(0, 1);
+            long sum = 0;
+            foreach (MatchCollection tokens in parsed)
+            {
+                var stack = new Stack<Expression>();
+                var groups = new Stack<Stack<Expression>>();
+                foreach (Match m in tokens)
+                {
+                    if (Regex.IsMatch(m.Value, @"\d+"))
+                    {
+                        if (stack.Count > 0)
+                        {
+                            var last = stack.Peek();
+                            switch (last)
+                            {
+                                case Addition add:
+                                    add.e2 = new Number(m.Value);
+                                    break;
+                                case Multiplication mult:
+                                    mult.e2 = new Number(m.Value);
+                                    break;
+                                default:
+                                    throw new NotImplementedException();
+                            }
+                        }
+                        else
+                        {
+                            stack.Push(new Number(m.Value));
+                        }
+                    }
+                    else if (m.Value.Equals("+"))
+                    {
+                        var last = stack.Pop();
+                        stack.Push(new Addition(last));
+                    }
+                    else if (m.Value.Equals("*"))
+                    {
+                        var last = stack.Pop();
+                        stack.Push(new Multiplication(last));
+                    }
+                    else if (m.Value.Equals("("))
+                    {
+                        groups.Push(stack);
+                        stack = new Stack<Expression>();
+                    }
+                    else if (m.Value.Equals(")"))
+                    {
+                        var last = stack.Pop();
+                        stack = groups.Pop();
+                        if (stack.Count > 0)
+                        {
+                            var peek = stack.Peek();
+                            switch (peek)
+                            {
+                                case Addition add:
+                                    add.e2 = new Group(last);
+                                    break;
+                                case Multiplication mult:
+                                    mult.e2 = new Group(last);
+                                    break;
+                                default:
+                                    stack.Push(new Group(last));
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            stack.Push(new Group(last));
+                        }
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                sum += stack.Pop().Eval();
+            }
+            Assert.AreNotEqual(486,sum );
+            Assert.AreEqual(31142189909908, sum);
         }
 
         [Test]
